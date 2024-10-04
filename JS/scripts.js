3feb8b078d5986f2cbbe860aa24e36e536d146cf
Taytay79 <114293801+Taytay79@users.noreply.github.com>
2024-10-04 14:55:38 -0300

    incluindo os arquivos do site

---
+++ JS/scripts.js
@@ -0,0 +1,18 @@
+let contador = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+correimagem();
+
+}, 5000);
+
+function correimagem(){
+contador++;
+if(contador > 4){
+ contador = 1;
+
+}
+
+document.getElementById('radio'+contador).checked = true;
+
+}
